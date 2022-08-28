0d5a7d1c6aa7f136aa4b5a69d348e751df0ed87f
wwwadminmd <40687566+wwwadminmd@users.noreply.github.com>
2022-08-28 17:13:50 +0300

    Add files via upload

---
+++ check.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+date=$(date)
+dir='/var/www/server.admin.md/backup/'
+srv1='SA'
+srv2='KAMAZ'
+
+###
+function srv1 (){
+
+if grep -s -q "$srv1" "$dir"results_*.j*; then
+    echo "$srv1 - SUCCESS! $date" >> "$dir"logs.txt
+else
+    echo "$srv1 - FAIL!!! $date" >> "$dir"logs.txt
+fi
+}
+###
+function srv2 (){
+
+if grep -s -q "$srv2" "$dir"results_*.j*; then
+    echo "$srv2 - SUCCESS! $date" >> "$dir"logs.txt
+else
+    echo "$srv2 - FAIL!!! $date" >> "$dir"logs.txt
+fi
+}
+###
+
+########################
+#
+srv1
+srv2
+#
+cat "$dir"logs.txt # | mail -e oleg.buian@gmail.comcat logs.txt
+rm -fr "$dir"results_* "$dir"logs*
