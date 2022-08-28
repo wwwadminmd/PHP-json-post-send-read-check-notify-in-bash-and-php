56ad60fdf132749818ca327fb9210a429894e691
wwwadminmd <40687566+wwwadminmd@users.noreply.github.com>
2022-08-28 17:10:04 +0300

    Add files via upload

---
+++ json-send.php
@@ -0,0 +1,25 @@
+<?php
+//The URL of the API.
+$url = 'http://localhost/backup/read.php';
+
+//Initiate cURL.
+$ch = curl_init($url);
+
+//The JSON data.
+$jsonData = 'KAMAZ OK';
+
+//Encode the array into JSON.
+$jsonDataEncoded = json_encode($jsonData);
+
+//Tell cURL that we want to send a POST request.
+curl_setopt($ch, CURLOPT_POST, 1);
+
+//Attach our encoded JSON string to the POST fields.
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+
+//Set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
+//Execute the request
+$result = curl_exec($ch);
+?>
